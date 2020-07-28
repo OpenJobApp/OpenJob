f41689af7b6ca3a78620821e571441c4918ac938
Sunny Dhoke <sunnydhoke22@gmail.com>
2020-07-28 22:41:34 +0530

    Added new screens
    Added
    - SignUpPage
    - SignInPage
    - AppUserRoleSelection

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import SignInPage from "./app/screens/SignInPage";
+
 import InitialLanguageSelection from "./app/screens/InitialLanguageSelection";
 
+import AppUserRoleSelection from "./app/screens/AppUserRoleSelection";
 export default function App() {
   return (
-    <InitialLanguageSelection />
+    <AppUserRoleSelection />
     /*
     <View style={styles.container}>
       <Text>Hi,This is OpenJobApp Team.</Text>
--- app/config/colors.js
+++ app/config/colors.js
@@ -1,4 +1,4 @@
 export default {
   primary: "blue",
-  secondary: "white",
+  secondary: "grey",
 };
